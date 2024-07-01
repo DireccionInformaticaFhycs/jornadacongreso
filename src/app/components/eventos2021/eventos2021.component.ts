3ef6fc86b85836258f1146ab45cccb4d78d10aa5
DireccionInformaticaFhycs <direccioninformatica@fhycs.unju.edu.ar>
2024-07-01 13:00:58 -0300

    actualizacion

--- src/app/components/eventos2021/eventos2021.component.ts
+++ src/app/components/eventos2021/eventos2021.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-eventos2021',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './eventos2021.component.html',
   styleUrl: './eventos2021.component.css'
 })
--- src/app/components/eventos2024/eventos2024.component.ts
+++ src/app/components/eventos2024/eventos2024.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-eventos2024',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './eventos2024.component.html',
   styleUrl: './eventos2024.component.css'
 })
